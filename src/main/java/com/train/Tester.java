9ea3819574923b44cc1cc9ec2eef2d040931bd3c
juanyuanzhang <40617047+juanyuanzhang@users.noreply.github.com>
2020-07-10 16:25:19 +0800

    Homework2 finish

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter number of tickets: ");
-        int totalTickets = scanner.nextInt();
-        System.out.print("How many round-trip tickets: ");
-        int rtTickets = scanner.nextInt();
-        Ticket ticket = new Ticket(totalTickets,rtTickets);
-        ticket.print();
+        int totalTickets = 0 ;
+        while(totalTickets != -1) {
+            System.out.print("Please enter number of tickets: ");
+            totalTickets = scanner.nextInt();
+            if(totalTickets != -1) {
+                System.out.print("How many round-trip tickets: ");
+                int rtTickets = scanner.nextInt();
+                Ticket ticket = new Ticket(totalTickets, rtTickets);
+                ticket.print();
+            }
+        }
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -4,18 +4,24 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    print("Please enter number of tickets: ")
-    var totalTickets = scanner.nextInt()
-    print("How many round-trip tickets: ")
-    var rtTickets = scanner.nextInt()
-    val ticket = TicketKotlin(totalTickets, rtTickets)
-    ticket.print()
+    var totalTickets = 0
+    while (totalTickets != -1 ) {
+        print("Please enter number of tickets: ")
+        totalTickets = scanner.nextInt()
+        if(totalTickets != -1){
+            print("How many round-trip tickets: ")
+            var rtTickets = scanner.nextInt()
+            val ticket = TicketKotlin(totalTickets, rtTickets)
+            ticket.print()
+        }
+    }
+
 }
 
 class TicketKotlin(var totalTickets: Int , var rtTickets: Int){
     fun print(){
-        println("Total tickets:"+totalTickets+"\n"+
-                "Round-trip:"+rtTickets+"\n"+
+        println("Total tickets: $totalTickets\n"+
+                "Round-trip: $rtTickets\n"+
                 "Total:"+((totalTickets-rtTickets)*1000+(rtTickets*2000*0.9)).toInt())
     }
 }
